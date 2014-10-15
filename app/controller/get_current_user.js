7e4ae959df9682259b08fa8b62e8674a9b5df549
bhekman <bradley.hekman@gmail.com>
2014-10-15 19:50:37 -0400

    Switched over to CeeVee template. Barely started the customizing.

--- app/controller/get_current_user.js
+++
@@ -1,6 +0,0 @@
-// Note that this function will become more complex when
-// non-local authentication methods are added in.
-module.exports = function getCurrentUser(req) {
-  return req.user.local.email;
-}
-
--- server.js
+++ server.js
@@ -5,21 +5,12 @@
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
 var flash    = require('connect-flash');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
-
-var configDB = require('./config/database.js');
-
-// configuration ===============================================================
-mongoose.connect(configDB.url); // connect to our database
-
-require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
@@ -28,17 +19,12 @@ app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs'); // set up ejs for templating
-
-// required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+app.engine('html', require('ejs').renderFile);
 
 app.use('/static', express.static(__dirname + '/views/static'));
 
 // routes ======================================================================
-require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
 app.listen(port);
