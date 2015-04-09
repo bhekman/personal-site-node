76dd852aff78ec24e1a52d9ba728f4319dfd1d56
bhekman <bradley.hekman@gmail.com>
2015-04-09 14:59:00 -0400

    Set up routing for honors page. Need to install Bootstrap next.

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,9 @@
 var url = require('url');
 
 module.exports = function(app, passport) {
+	app.get('/honors', function(req, res) {
+		res.render('honors.html');
+	});
 	app.get('/', function(req, res) {
 		res.render('index.html');
 	});
