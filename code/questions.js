8c0f1e703414bb375d6e12baa1bf774582ab8552
bhekman <bradley.hekman@gmail.com>
2015-04-16 08:06:32 -0400

    More questions.

--- code/questions.js
+++ code/questions.js
@@ -19,7 +19,7 @@ module.exports = function(mongoose, models) {
   new models.Question({
       num: 1,
       quiz: ['misc'],
-      question: "Does background music disrupt cognitive performance on some learning tasks?",
+      question: "Does background music disrupt cognitive performance while learning?",
       correct_choice: 2,
       choices: ["Yes. Music disrupts studying",
                 "No. Music does not disrupt studying. It can even improve recall",
@@ -36,13 +36,14 @@ module.exports = function(mongoose, models) {
   /////////////////
   footer = "";
   footer += "<h4>Explanation</h4>";
-  footer += "<div>Although sleep deprivation does dramatically affect performance on basic and routine tasks, it also precludes memory consolidation during sleep. In this way, sleep deprivation costs double: acuity and vigilance are lost, and no benefit is gained.</div>";
+  footer += "<div>This answer is a strong \"Yes.\" This has been shown by many studies. Lack of sleep prevents consolidation of memories and impair performance while awake. Lack of sleep especially affects performance on routine and basic tasks.</div>";
   footer += "<br>";
   footer += "<h4>Further reading</h4><ul>";
   footer += "<li><a href=\"http://www.neurology.org/content/64/7/E25.full\">(Research) Cognitive benefits of sleep and their loss due to sleep deprivation</a></li>";
   footer += "<li><a href=\"http://journals.lww.com/academicmedicine/Abstract/1991/11000/A_review_of_studies_concerning_effects_of_sleep.13.aspx\">(Summary of Research) A review of studies concerning effects of sleep deprivation and fatigue on residents\\' performance.</a></li>";
   footer += "<li><a href=\"http://physrev.physiology.org/content/93/2/681.abstract\">(Research) About Sleep\\'s Role in Memory</a></li>";
   footer += "<li><a href=\"http://learnmem.cshlp.org/content/11/6/679.short\">(Research) Declarative memory consolidation: Mechanisms acting during human sleep</a></li>";
+  footer += "<li><a href=\"http://www.smrv-journal.com/article/S1087-0792%2805%2900123-1/abstract\">(Research) Sleep loss, learning capacity and academic performance</a></li>";
   footer += "</ul>";
   new models.Question({
       num: 2,
@@ -211,7 +212,7 @@ module.exports = function(mongoose, models) {
   /////////////////
   footer = "";
   footer += "<h4>Explanation</h4>";
-  footer += "Unfortunately, highlighting while reading provides minimal benefit to the learner. Additionally, rereading, summarizing, and underlining were found to be of low utility. These results have been confirmed by multiple studies.";
+  footer += "<div>Unfortunately, highlighting while reading provides minimal benefit to the learner. Additionally, rereading, summarizing, and underlining were found to be of low utility. These results have been confirmed by multiple studies.</div>";
   footer += "<br>";
   footer += "<h4>Further reading</h4><ul>";
   footer += "<li><a href=\"http://search.proquest.com/docview/305314990\">(Research) Investigating the effects of text annotation on student retention rates</a></li>";
@@ -259,12 +260,118 @@ module.exports = function(mongoose, models) {
       footer: footer
   }).save();
 
+  //////////////////
+  // Question #10 //
+  //////////////////
+  footer = "";
+  footer += "<h4>Explanation</h4>";
+  footer += "<div>Multiple studies have shown that daytime napping provides similar benefits to nighttime sleep. Daytime naps appear to provide benefit by repairing fatigue and ehnancing memory consolidation</div><br>";
+  footer += "<div>Most observational studies, however, have shown an unsurprising correlations between daytime napping, sleep deprivation and poor academic performance. The detriments from sleep deprivation clearly outweigh the benefits of napping.</div>";
+  footer += "<br>";
+  footer += "<h4>Further reading</h4><ul>";
+  footer += "<li><a href=\"http://onlinelibrary.wiley.com.proxy.lib.umich.edu/doi/10.1111/ejn.12118/abstract\"><(Research) Napping to renew learning capacity: enhanced encoding after stimulation of sleep slow oscillations</a></li>";
+  footer += "<li><a href=\"http://dx.doi.org/10.1111/j.1365-2869.2008.00718.x\">(Research) Benefits of napping in healthy adults: impact of nap length, time of day, age, and experience with napping</a></li>";
+  footer += "<li><a href=\"http://search.proquest.com.proxy.lib.umich.edu/docview/1626535113?pq-origsite=summon\">(Research) The Napping Behaviour of Australian University Students</a></li>";
+  footer += "<li><a href=\"http://www.nature.com.proxy.lib.umich.edu/neuro/journal/v5/n7/full/nn0702-618.html\">(Article, Nature) Be caught napping: you\\'re doing more than resting your eyes</a></li>";
+  footer += "</ul>";
+  new models.Question({
+      num: 10,
+      quiz: ['sleep'],
+      question: "Is daytime napping generally good or bad for learning?",
+      correct_choice: 0,
+      choices: ["Bad. Napping actually causes more sleepiness than it fixes.",
+                "Good. Napping rests the brain and rejuvinates it.",
+                "Maybe. The research is inconclusive or contradictory.",
+                "I don’t know."],
+      num_choices: 4,
+      footer: footer
+  }).save();
+
+  //////////////////
+  // Question #11 //
+  //////////////////
+  footer = "";
+  footer += "<h4>Explanation</h4>";
+  footer += "<div>Health authorities haven\\'t reached a concensus on this figure, but most recommend a maximum dosage around 300 mg per day. At dosages higher than this, side effects such as an upset stomach or irritability often appear.</div><br>";
+  footer += "<div>A measure called an LD50 is used to describe dosages that kill 50% of the population. The projected LD50 for caffeine in 75kg adults is equivalent to approximately 118 cups of coffee (28.3 liters), according to compoundchem.com. Note also that someone drinking that much coffee would die from drinking too much liquid before dying from the caffeine. Pleasant!</div>";
+  footer += "<br>";
+  footer += "<h4>Further reading</h4><ul>";
+  footer += "<li><a href=\"url\">title</a></li>";
+  footer += "<li><a href=\"http://en.wikipedia.org/wiki/Health_effects_of_caffeine#Negative_effects\">(Wikipedia) Health Effects of Caffeine</a></li>";
+  footer += "<li><a href=\"http://www.livestrong.com/article/471513-the-maximum-daily-dosage-of-caffeine/\">(Webpage, Livestrong) The Maximum Daily Dosage of Caffeine</a></li>";
+  footer += "<li><a href=\"http://www.mayoclinic.org/healthy-lifestyle/nutrition-and-healthy-eating/in-depth/caffeine/art-20045678\">(Webpage, Mayo Clinic) Caffeine: How much is too much?</a></li>";
+  footer += "<li><a href=\"http://www.compoundchem.com/2014/07/27/lethaldoses/\">(Webpage) Lethal Doses of Common Chemicals</a></li>";
+  footer += "</ul>";
+  new models.Question({
+      num: 11,
+      quiz: ['sleep'],
+      question: "How much caffeine is considered \"safe\" for a healthy adult human to consume per day?",
+      correct_choice: 0,
+      choices: ["300 mg",
+                "600 mg",
+                "1000 mg",
+                "I don’t know."],
+      num_choices: 4,
+      footer: footer
+  }).save();
+
+  //////////////////
+  // Question #12 //
+  //////////////////
+  footer = "";
+  footer += "<h4>Explanation</h4>";
+  footer += "<div>Studies have indeed shown a positive correlation between eating breakfast and GPA. Additional studies have tried to identify what type of breakfast is most beneficial, but haven\\'t found any strong results (nutrition is complex).";
+  footer += "<br>";
+  footer += "<h4>Further reading</h4><ul>";
+  footer += "<li><a href=\"http://digitalscholarship.tnstate.edu/dissertations/AAI3024637/\">(Research) Breakfast consumption and student achievement prior to lunch</a></li>";
+  footer += "<li><a href=\"http://search.proquest.com/docview/304312798\">(Research) The effect of breakfast eating on academic achievement among developmental college students</a></li>";
+  footer += "<li><a href=\"http://search.proquest.com/docview/1476200680\">(Research) Glycemic load of whole grains, refined grains, and simple sugars consumed at breakfast: Impact on GPA in college students</a></li>";
+  footer += "</ul>";
+  new models.Question({
+      num: 7,
+      quiz: ['misc'],
+      question: "Supposedly, eating breakfast is beneficial and important. Has it actually been shown to improve academic performance though?",
+      correct_choice: 0,
+      choices: ["Yes. It's science.",
+                "No. Nutrition is important overall, but skipping breakfast doesn't matter.",
+                "Maybe. The research is inconclusive or contradictory.",
+                "I don’t know."],
+      num_choices: 4,
+      footer: footer
+  }).save();
+
+  //////////////////
+  // Question #13 //
+  //////////////////
+  footer = "";
+  footer += "<h4>Explanation</h4>";
+  footer += "Out of class cell phone usage does correlate with a worse GPA. Correlation does not imply causation however, so you don\\'t necessarily need to destroy your phone.";
+  footer += "<br>";
+  footer += "<h4>Further reading</h4><ul>";
+  footer += "<li><a href=\"http://go.galegroup.com.proxy.lib.umich.edu/ps/i.do?id=GALE%7CA270894540&v=2.1&u=lom_umichanna&it=r&p=AONE&sw=w\">(Research) Cell phone use and grade point average among undergraduate university students</a></li>";
+  footer += "<li><a href=\"http://jmd.sagepub.com.proxy.lib.umich.edu/content/35/1/26\">(Research) An Introduction to Multitasking and Texting: Prevalence and Impact on Grades and GPA in Marketing Classes</a></li>";
+  footer += "<li><a href=\"http://www.sciencedirect.com/science/article/pii/S0747563213003993\">(Research) The relationship between cell phone use, academic performance, anxiety, and Satisfaction with Life in college students</a></li>";
+  footer += "</ul>";
+  new models.Question({
+      num: 13,
+      quiz: ['misc'],
+      question: "It is generally agreed that cell phone usage in class hurts GPA. But cell phone usage outside of class doesn't correlate with a worse GPA, right?",
+      correct_choice: 1,
+      choices: ["Right. Only in-class usage is detrimental. Out-of-class usage indicates a healthy social life.",
+                "Wrong. Out-of-class cell phone usage also correlates with a worse GPA.",
+                "Maybe. The research is inconclusive or contradictory.",
+                "I don’t know."],
+      num_choices: 4,
+      footer: footer
+  }).save();
+
+
+  /*
   ///////////
   // TEMPLATE
   /////////////////
   // Question #7 //
   /////////////////
-  /*
   footer = "";
   footer += "<h4>Explanation</h4>";
   footer += "";
